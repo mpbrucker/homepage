90a58fe2172afe6e3b25840b6c0cee32a9c8edc3
Matt Brucker <mpbrucker@gmail.com>
2018-09-16 00:05:33 -0400

    Implementing lambda function.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,16 +1,63 @@
-import json
+import boto3
+import PIL
+from PIL import Image
+from io import BytesIO
+from os import path
+
+s3 = boto3.resource('s3')
+origin_bucket = 'mbrucker.com'
+destination_bucket = 'my-resized'
+width_size = 1000
+height_size = 500
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        "statusCode": 200,
-        "body": json.dumps('Hello from Lambda!')
-    }
-import json
+    
+    for key in event.get('Records'):
+        object_key = key['s3']['object']['key']
+        extension = path.splitext(object_key)[1].lower()
 
-def lambda_handler(event, context):
-    # TODO implement
-    return {
-        "statusCode": 200,
-        "body": json.dumps('This is a test!')
-    }
+        # Grabs the source file
+        obj = s3.Object(
+            bucket_name=origin_bucket,
+            key=object_key,
+        )
+        obj_body = obj.get()['Body'].read()
+    
+        # Checking the extension and
+        # Defining the buffer format
+        if extension in ['.jpeg', '.jpg']:
+            format = 'JPEG'
+        if extension in ['.png']:
+            format = 'PNG'
+
+        # Resizing the image
+        img = Image.open(BytesIO(obj_body))
+
+        # If height is over the threshold, resize by height
+        if img.size[1] > height_size:
+            wresize = (height_size / float(img.size[1]))
+            width = int(float(img.size[0]) * float(wresize))
+            img = img.resize((width, height_size), PIL.Image.ANTIALIAS)
+            
+        elif img.size[0] > width_size:
+            hresize = (width_size / float(img.size[0]))
+            height = int((float(img.size[1]) * float(hresize)))
+            img = img.resize((width_size, height), PIL.Image.ANTIALIAS)
+
+        buffer = BytesIO()
+        img.save(buffer, format)
+        buffer.seek(0)
+
+        # Uploading the image
+        obj = s3.Object(
+            bucket_name=destination_bucket,
+            key='images/' + object_key,
+        )
+        obj.put(Body=buffer)
+
+        # Printing to CloudWatch
+        print('File saved at {}/{}'.format(
+            destination_bucket,
+            object_key,
+        ))
