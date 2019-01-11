a1660d31c0638b44a8d63b1d7e10966a14c204a3
Matthew Brucker <mpbrucker@gmail.com>
2019-01-11 11:25:55 -0500

    Adding new SVG movement.

---
+++ assets/js/animate-text.js
@@ -0,0 +1,12 @@
+// Setting up the options
+var options = {
+    startLength: 0, // start positioning the slider items at the beginning of the SVG path
+    duration: 10, // animation duration (used by anime.js)
+    stagger: 0, // incrementally delays among items, producing an staggering effect
+    easing: 'easeOutElastic', // easing function (used by anime.js)
+    elasticity: 100, // elasticity factor (used by anime.js)
+    rotate: true // This indicates that items should be rotated properly to match the SVG path curve
+};
+
+// Initialize the slider using our SVG path, items, and options
+new PathSlider('#text-path', '#path-item', options);
