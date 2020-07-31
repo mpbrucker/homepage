8144308c538c44199d46ccf1e54089bdfc9a4139
Matt Brucker <mpbrucker@gmail.com>
2020-07-31 15:09:01 -0400

    Adding test custom shader component

--- assets/js/aframe-components.js
+++ assets/js/aframe-components.js
@@ -1,3 +1,45 @@
+function vertexShader() {
+    return `
+    varying vec3 vUv; 
+
+    void main() {
+      vUv = position; 
+
+      vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
+      gl_Position = projectionMatrix * modelViewPosition; 
+    }
+    `
+}
+
+function fragmentShader() {
+    return `
+    uniform vec3 colorA; 
+    uniform vec3 colorB; 
+    varying vec3 vUv;
+
+    void main() {
+        gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
+    }
+    `
+}
+
+AFRAME.registerComponent('custom-shader', {
+    init: function() {
+        let uniforms = {
+            colorB: {type: 'vec3', value: new THREE.Color(0xACABE5)},
+            colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)}
+        };
+
+        let material =  new THREE.ShaderMaterial({
+            uniforms: uniforms,
+            fragmentShader: fragmentShader(),
+            vertexShader: vertexShader(),
+        });
+
+        this.el.getObject3D('mesh').material = material;
+    }
+})
+
 AFRAME.registerComponent('point-at-camera', {
     // schema: {default: 0.5}
 
@@ -17,15 +59,14 @@ AFRAME.registerComponent('point-at-camera', {
             this.mousePos.set(
                 ( event.clientX / window.innerWidth ) * 2 - 1,
                 - ( event.clientY / window.innerHeight ) * 2 + 1,
-                0.5 );
-            this.mousePos.unproject(this.camera);
-            this.mousePos.sub( this.camera.position ).normalize();
+                -5 );
+            // this.mousePos.unproject(this.camera);
+            // this.mousePos.sub( this.camera.position ).normalize();
 
 
-            let distance = (-5 - this.camera.position.z) / this.mousePos.z;
+            // let distance = (-1- this.camera.position.z) / this.mousePos.z;
 
-            this.pos.copy( this.camera.position ).add( this.mousePos.multiplyScalar( distance ) );
-            console.log(this.pos);
+            this.pos.copy( this.camera.position ).add( this.mousePos.multiplyScalar( 100 ) );
             this.el.object3D.lookAt(this.pos);
         }
     }
