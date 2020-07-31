931f700d2ebf7e37e7c926954eb3dac495eecae6
Matt Brucker <mpbrucker@gmail.com>
2020-07-31 14:35:17 -0400

    Adding aframe JS components and layout

---
+++ assets/js/aframe-components.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent('point-at-camera', {
+    init: function() {
+        console.log('init');
+        this.raycaster = new THREE.Raycaster();
+        this.mousePos = new THREE.Vector3();
+        this.pos = new THREE.Vector3();
+        this.camera = document.querySelector("[camera]").getObject3D('camera');
+        console.log(this.camera)
+
+        document.addEventListener('mousemove', (e) => this.update(e));
+    },
+
+    update: function(e) {
+        if (e) {
+            this.mousePos.set(
+                ( event.clientX / window.innerWidth ) * 2 - 1,
+                - ( event.clientY / window.innerHeight ) * 2 + 1,
+                0.5 );
+            this.mousePos.unproject(this.camera);
+            this.mousePos.sub( this.camera.position ).normalize();
+
+
+            let distance = (0.1- this.camera.position.z) / this.mousePos.z;
+
+            this.pos.copy( this.camera.position ).add( this.mousePos.multiplyScalar( distance ) );
+        }
+    }
+})
--- index.md
+++ index.md
@@ -2,6 +2,7 @@
 layout: home
 title: Home
 description: Home
+aframeEnabled: true
 ---
 
 # Matt Brucker
